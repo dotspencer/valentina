66da4aedf74c2bc62f47f69096be0f8bb754edde
Spencer Smith <dontaskspencer@gmail.com>
2017-02-02 22:42:43 -0700

    Pre-heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import path from 'path'
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 const router = express.Router()
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+app.use(staticFiles)
+
 router.get('/cities', (req, res) => {
   const cities = [
     {name: 'New York City', population: 8175133},
@@ -17,8 +22,10 @@ router.get('/cities', (req, res) => {
 
 app.use(router)
 
-app.set('port', (process.env.PORT || 3001))
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles)
 
+app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
